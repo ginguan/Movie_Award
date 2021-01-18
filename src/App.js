d3422564ae63e2dcfdd6a22489a9674c5380321e
ginguan <yguan074@uottawa.ca>
2021-01-17 23:17:19 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Search from 'components/search.js';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Search/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Form} from 'react-bootstrap';
+
+function Search(props){
+    return (
+        <div className="search-bar-container justify-content-center md-form  active-cyan-2 mt-2">
+            <Form className=" search-bar">
+                <Form.Group className = " md-form form-sm active-pink-2 mt-2" controlId="formBasicEmail">
+                    <Form.Label className="search-results-placeholder">Search Movie Titles</Form.Label>
+                    <Form.Control className="search-control"placeholder="Enter Movie Title" value={props.searchValue} onChange={event => onChangeForm(event)} />
+                    <Form.Text className="text-muted"  >
+                       
+                </Form.Text>
+                </Form.Group>
+            </Form>
+        </div>
+    )
+    
+}
+
+export default Search;
